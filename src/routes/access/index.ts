26ab4084756a368ce173e97661a01c94db6312b4
khanhdang2911 <khanhdang3152@gmail.com>
2024-10-20 20:12:44 +0700

    feat: build api key for app

--- src/routes/access/index.ts
+++ src/routes/access/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { signUp } from "../../controllers/access.controller";
+import { createApiKey } from "../../controllers/apiKey.controller";
 const accessRouter = express.Router();
 
+
 accessRouter.post("/access/signUp", signUp);
+accessRouter.post("/access/createApiKey", createApiKey);
 
 export default accessRouter;
--- src/services/keyToken.service.ts
+++ src/services/keyToken.service.ts
@@ -1,33 +1,31 @@
 import { Schema } from "mongoose";
 import { KeyToken } from "../models/keyToken.model";
-class KeyTokenService {
-  public static async createKeyToken(
-    user: Schema.Types.ObjectId,
-    privateKey: string,
-    publicKey: string,
-    refreshToken: string
-  ) {
-    try {
-      const newKeyToken = await KeyToken.create({
-        user,
-        privateKey,
-        publicKey,
-        refreshToken,
-      });
-      if (newKeyToken) {
-        return {
-          success: true,
-          message: "KeyToken created successfully",
-          data: newKeyToken,
-        };
-      }
-    } catch (error) {
+const createKeyToken = async (
+  user: Schema.Types.ObjectId,
+  privateKey: string,
+  publicKey: string,
+  refreshToken: string
+) => {
+  try {
+    const newKeyToken = await KeyToken.create({
+      user,
+      privateKey,
+      publicKey,
+      refreshToken,
+    });
+    if (newKeyToken) {
       return {
-        success: false,
-        message: error,
+        success: true,
+        message: "KeyToken created successfully",
+        data: newKeyToken,
       };
     }
+  } catch (error) {
+    return {
+      success: false,
+      message: error,
+    };
   }
-}
+};
 
-export default KeyTokenService;
+export { createKeyToken };
